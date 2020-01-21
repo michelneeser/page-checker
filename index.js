d3c698933ce4c3bad6400b8a7204564813af7171
michelneeser <github@michelneeser.ch>
2020-01-21 21:46:22 +0100

    search on entire page / enable execution on AWS Lambda

--- index.js
+++ index.js
@@ -3,36 +3,44 @@ const path = require('path');
 const util = require('util');
 const puppeteer = require('puppeteer');
 
-const check = async (url, selector, textToCheck) => {
+const checkSingleUrl = async (url, selector, textToCheck) => {
 
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url);
 
   const text = await page.evaluate((selector) => {
-    return document.querySelector(selector).textContent;
+    if (selector) {
+      return document.querySelector(selector).textContent;
+    } else {
+      return document.body.innerHTML;
+    }
   }, selector);
 
   await browser.close();
 
-  return text === textToCheck;
+  return text.includes(textToCheck);
 };
 
-(async () => {
+const check = async () => {
   const readFile = util.promisify(fs.readFile);
   let checks = await readFile(path.join(__dirname, 'checks.json'));
   if (checks) {
     checks = JSON.parse(checks);
   }
 
-  checks.forEach(async ({ url, selector, textToCheck }) => {
-    console.log(`checking ${url}...`);
-    const checkResult = await check(url, selector, textToCheck);
+  checks.forEach(async ({ name, url, selector, textToCheck }) => {
+    console.log(`checking ${name} (${url.substring(0, 50)})...`);
+    const checkResult = await checkSingleUrl(url, selector, textToCheck);
 
     if (checkResult) {
-      console.log(`text ${textToCheck} IS there`);
+      console.log(`==> ${name} SUCCESS: text "${textToCheck}" IS there`);
     } else {
-      console.log(`text ${textToCheck} is NOT there`);
+      console.log(`==> ${name} FAIL: text "${textToCheck}" IS NOT there`);
     }
   });
-})();
+};
+
+check();
+
+exports.handler = check; // for AWS Lambda
